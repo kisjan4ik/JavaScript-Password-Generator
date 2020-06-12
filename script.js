9826ab596472844ffb395e8538b2786b6611d284
Turetska <oturetska@yahoo.com>
2020-06-11 20:28:00 -0400

    added alerts, confirms and promts

--- script.js
+++ script.js
@@ -1,6 +1,61 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lcLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var ucLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numSymbols = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specSymbols = ["~", "`", "!", "#", "$", "%", "^", "&", "*", "+", "=", "-", "[", "]", ";", ",", "/", "{", "}", "|", ":", "<", ">", "?"];
+
+function generatePassword() {
+
+
+  var array = []
+
+  var numOfChar = parseInt(prompt("How many characters would you like your password to contain?")) 
+  
+  if (isNaN(numOfChar)) {
+    alert("The password length must be provided as a number!")
+  }
+  if (numOfChar < 8) {
+    alert("Password length must be at least 8 characters!")
+  }
+  if (numOfChar > 128) {
+    alert("Password length must be less than 129 characters!")
+  }
+ 
+  var lcYes = confirm ("Click OK to confirm including lower case letters");
+  if (lcYes == true) {
+    array.push(lcLetters)
+  }
+
+  var ucYes = confirm("Click OK to confirm including upper case letters");
+  if (ucYes == true) {
+    array.push(ucLetters)
+  }
+
+  var numYes = confirm ("Click OK to confirm including numeric charactres");
+  if (numYes == true) {
+    array.push(numSymbols)
+  }
+
+  var specYes = confirm ("Click OK to confirm including special charactres");
+  if (specYes == true){
+    array.push(specSymbols)
+  }
+}
+
+
+
+
+
+
+
+
+
+//   return  numOfChar
+// }
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
